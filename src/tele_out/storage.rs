4b541022fc626bfd592378712d9e37bf360336dd
HAOYUatHZ <haoyu@protonmail.com>
2021-06-07 21:22:33 +0800

    init MIGRATOR

--- src/tele_out/storage.rs
+++ src/tele_out/storage.rs
@@ -3,6 +3,10 @@ pub type ConnectionType = sqlx::postgres::PgConnection;
 pub type PoolOptions = sqlx::postgres::PgPoolOptions;
 pub type DBErrType = sqlx::Error;
 
+static MIGRATOR: sqlx::migrate::Migrator = sqlx::migrate!(); // defaults to "./migrations"
+
 pub async fn from_config() -> anyhow::Result<sqlx::Pool<DbType>> {
     unimplemented!();
+
+    // MIGRATOR.run();
 }
