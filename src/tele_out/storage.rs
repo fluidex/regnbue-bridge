86c6bc411f0b45cee03bff01bb4faa7a14a72a4d
HAOYUatHZ <haoyu@protonmail.com>
2021-06-08 15:12:15 +0800

    cargo clippy

--- src/tele_out/storage.rs
+++ src/tele_out/storage.rs
@@ -3,12 +3,13 @@ use crate::tele_out::Settings;
 pub type DbType = sqlx::Postgres;
 pub type ConnectionType = sqlx::postgres::PgConnection;
 pub type PoolOptions = sqlx::postgres::PgPoolOptions;
-pub type DBErrType = sqlx::Error;
+pub type PoolType = sqlx::Pool<DbType>;
+pub type DbErrType = sqlx::Error;
 
 // TODO: config this filepath
 static MIGRATOR: sqlx::migrate::Migrator = sqlx::migrate!("./migrations/tele_out");
 
-pub async fn from_config(config: &Settings) -> anyhow::Result<sqlx::Pool<DbType>> {
+pub async fn from_config(config: &Settings) -> anyhow::Result<PoolType> {
     let db_pool = PoolOptions::new().connect(&config.db).await?;
 
     MIGRATOR.run(&db_pool).await?;
