6fbb5f3b0f988f10f59760f8e0eed1c30b0c0672
HAOYUatHZ <haoyu@protonmail.com>
2021-06-12 14:10:12 +0800

    init spawn

--- src/tele_in/config.rs
+++ src/tele_in/config.rs
@@ -1,6 +1,15 @@
 use serde::Deserialize;
+use std::time::Duration;
 
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 pub struct Settings {
+    pub send_interval: u64,
     pub db: String,
+}
+
+impl Settings {
+    /// Converts `self.interval` into `Duration`.
+    pub fn send_interval(&self) -> Duration {
+        Duration::from_millis(self.send_interval)
+    }
 }
--- src/tele_in/tx_sender.rs
+++ src/tele_in/tx_sender.rs
@@ -1,17 +1,37 @@
 use crate::storage::PoolType;
 use crate::tele_in::Settings;
+use std::time::Duration;
 
 #[derive(Debug)]
 pub struct TxSender {
     connpool: PoolType,
+    send_interval: Duration,
 }
 
 impl TxSender {
-    pub fn from_config_with_pool(_config: &Settings, connpool: PoolType) -> Self {
-        Self { connpool }
+    pub fn from_config_with_pool(config: &Settings, connpool: PoolType) -> Self {
+        Self {
+            connpool,
+            send_interval: config.send_interval(),
+        }
     }
 
     pub fn run(&self) -> tokio::task::JoinHandle<()> {
+        let mut timer = tokio::time::interval(self.send_interval);
+
+        // TODO: use worker_pool for multiple workers
+        tokio::spawn(async move {
+            loop {
+                timer.tick().await;
+                log::debug!("ticktock!");
+                if let Err(e) = self.clone().run_inner().await {
+                    log::error!("{}", e);
+                };
+            }
+        })
+    }
+
+    async fn run_inner(&self) -> Result<(), anyhow::Error> {
         unimplemented!()
     }
 }
