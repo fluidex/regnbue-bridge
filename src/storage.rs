c6039e2ca67886d38f0fc0ae131089d1c6750859
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2021-06-12 12:37:14 +0800

    add InternalTx model type (#12)

--- src/storage.rs
+++ src/storage.rs
@@ -3,3 +3,6 @@ pub type ConnectionType = sqlx::postgres::PgConnection;
 pub type PoolOptions = sqlx::postgres::PgPoolOptions;
 pub type PoolType = sqlx::Pool<DbType>;
 pub type DbErrType = sqlx::Error;
+
+pub type TimestampDbType = chrono::NaiveDateTime;
+pub type DecimalDbType = rust_decimal::Decimal;
--- src/tele_in/storage/mod.rs
+++ src/tele_in/storage/mod.rs
@@ -1,3 +1,5 @@
 use crate::storage::{PoolOptions, PoolType};
 
+pub mod models;
+
 static MIGRATOR: sqlx::migrate::Migrator = sqlx::migrate!("./migrations/tele_in");
