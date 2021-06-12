b39ef7a5cd109b87a2e706ecb69047fe523ba461
HAOYUatHZ <haoyu@protonmail.com>
2021-06-12 12:33:18 +0800

    add InternalTx type

---
+++ src/tele_in/storage/models.rs
@@ -0,0 +1,23 @@
+use serde::{Deserialize, Serialize};
+
+pub mod tablenames {
+    pub const InternalTx: &str = "internal_tx";
+}
+
+#[derive(sqlx::Type, Debug, Clone, Serialize)]
+#[sqlx(type_name = "tx_status", rename_all = "snake_case")]
+pub enum TxStatus {
+    Proposed,
+    Sent,
+    Confirmed,
+}
+
+#[derive(sqlx::FromRow, Debug, Clone, Serialize)]
+pub struct InternalTx {
+    pub id: TimestampDbType,
+    pub to_user: i32,
+    pub asset: String,
+    pub amount: DecimalDbType,
+    pub created_time: TimestampDbType,
+    pub updated_time: TimestampDbType,
+}
