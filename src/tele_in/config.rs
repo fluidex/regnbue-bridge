1e10d4bb58ecf5d14a1afaad106f66656d9556e0
HAOYUatHZ <haoyu@protonmail.com>
2021-06-12 11:16:53 +0800

    init settings

---
+++ src/tele_in/config.rs
@@ -0,0 +1,6 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+pub struct Settings {
+    pub db: String,
+}
--- src/tele_in/mod.rs
+++ src/tele_in/mod.rs
@@ -1,1 +1,5 @@
 pub mod storage;
+pub mod config;
+
+
+pub use config::Settings;
