7f1af74c2e1e8f1ac82e2cb144d9117b42768424
HAOYUatHZ <haoyu@protonmail.com>
2021-07-09 11:02:35 +0800

    first attempt

--- src/faucet/msg.rs
+++ src/faucet/msg.rs
@@ -12,9 +12,8 @@ pub fn load_msgs_from_mq(
 ) -> Option<std::thread::JoinHandle<anyhow::Result<()>>> {
     let brokers = brokers.to_owned();
     Some(std::thread::spawn(move || {
-        let rt: tokio::runtime::Runtime = tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap();
-
         let writer = MessageWriter { sender };
+        let rt: tokio::runtime::Runtime = tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap();
         rt.block_on(async move {
             let consumer: StreamConsumer = rdkafka::config::ClientConfig::new()
                 .set("bootstrap.servers", brokers)
@@ -25,22 +24,13 @@ pub fn load_msgs_from_mq(
                 .set("auto.offset.reset", "earliest")
                 .create()
                 .unwrap();
-
             let consumer = std::sync::Arc::new(consumer);
-            loop {
-                let cr_main = SimpleConsumer::new(consumer.as_ref())
-                    .add_topic(UNIFY_TOPIC, Simple::from(&writer))
-                    .unwrap();
-
-                tokio::select! {
-                    _ = tokio::signal::ctrl_c() => {
-                        log::warn!("Ctrl-c received, shutting down");
-                        break;
-                    },
-
-                    err = cr_main.run_stream(|cr|cr.stream()) => {
-                        log::error!("Kafka consumer error: {}", err);
-                    }
+            let cr_main = SimpleConsumer::new(consumer.as_ref())
+                .add_topic(UNIFY_TOPIC, Simple::from(&writer))
+                .unwrap();
+            tokio::select! {
+                err = cr_main.run_stream(|cr|cr.stream()) => {
+                    log::error!("Kafka consumer error: {}", err);
                 }
             }
         });
