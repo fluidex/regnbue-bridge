52f77465fd871e68fab6eea2c432448278bddf3e
HAOYUatHZ <haoyu@protonmail.com>
2021-07-13 17:35:08 +0800

    fix

--- src/tele_out/types.rs
+++ src/tele_out/types.rs
@@ -8,6 +8,6 @@ pub enum ContractCall {
 #[derive(Debug)]
 pub struct ProofData {
     pub block_id: U256,
-    pub public_inputs: U256,
-    pub serialized_proof: U256,
+    pub public_inputs: Vec<U256>,
+    pub serialized_proof: Vec<U256>,
 }
