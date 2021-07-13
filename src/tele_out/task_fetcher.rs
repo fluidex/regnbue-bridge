1ca49027cb80e6e3b087a8bae9038b9124b41c73
HAOYUatHZ <haoyu@protonmail.com>
2021-07-13 17:21:43 +0800

    refactor

--- src/tele_out/task_fetcher.rs
+++ src/tele_out/task_fetcher.rs
@@ -13,25 +13,35 @@ impl TaskFetcher {
         Self { connpool }
     }
 
-    // TODO: better type binding
     pub async fn run(&self, tx: Sender<ContractCall>) {
-        // TODO: ticker loop
+        let mut timer = tokio::time::interval(self.send_interval);
 
+        loop {
+            timer.tick().await;
+            log::debug!("ticktock!");
+
+            if let Err(e) = self.run_inner(tx).await {
+                log::error!("{}", e);
+            };
+        }
+    }
+
+    async fn run_inner(&self, tx: Sender<ContractCall>) -> Result<(), anyhow::Error>{
         let query = format!("select * from {} where status = $1 LIMIT 1", models::tablenames::TASK);
-        // TODO: error handling
         let task: Option<models::Task> = sqlx::query_as(&query)
             .bind(models::TaskStatus::Proved)
             .fetch_optional(&self.connpool)
-            .await
-            .unwrap();
+            .await?;
+
         if task.is_some() {
             let task = task.unwrap();
             tx.try_send(ContractCall::SubmitProof(ProofData {
                 block_id: task.block_id.into(),
                 public_inputs: vec![],
                 serialized_proof: vec![],
-            }))
-            .unwrap();
+            }))?;
         }
+
+        Ok(())
     }
 }
