d9830747ca91cfa906012633ecd75749e15b1614
HAOYUatHZ <haoyu@protonmail.com>
2021-07-13 14:54:17 +0800

    fix

--- src/tele_out/eth_sender/mod.rs
+++ src/tele_out/eth_sender/mod.rs
@@ -14,14 +14,14 @@ use std::convert::TryFrom;
 #[derive(Debug)]
 pub struct EthSender {
     connpool: PoolType,
-    contract: Contract,
+    contract: Contract<Provider<Http>>,
 }
 
 impl EthSender {
     pub fn from_config_with_pool(config: &Settings, connpool: PoolType) -> Result<Self, anyhow::Error> {
         let address = config.contract_address.parse::<Address>()?;
         let abi: Abi = contracts::get_abi(&config.contract_abi_file_path)?;
-        let client = Provider::<Http>::try_from(&config.web3_url)?; // assume wallet inside
+        let client = Provider::<Http>::try_from(config.web3_url.as_str())?; // assume wallet inside
         let contract = Contract::new(address, abi, client);
 
         Ok(Self { connpool, contract })
