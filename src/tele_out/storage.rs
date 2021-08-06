e5cb2400d34b1565442a8e61dd2b9ec7f4a9f3dc
lightsing <light.tsing@gmail.com>
2021-08-06 17:35:52 +0800

    remove migrator

--- src/tele_out/storage.rs
+++ src/tele_out/storage.rs
@@ -1,12 +1,8 @@
 use crate::storage::{PoolOptions, PoolType};
 use crate::tele_out::Settings;
 
-static MIGRATOR: sqlx::migrate::Migrator = sqlx::migrate!("./migrations/tele_out");
-
 pub async fn from_config(config: &Settings) -> anyhow::Result<PoolType> {
     let db_pool = PoolOptions::new().connect(&config.db).await?;
-
-    MIGRATOR.run(&db_pool).await?;
 
     Ok(db_pool)
 }
