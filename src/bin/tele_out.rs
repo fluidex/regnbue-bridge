ced19ca81735fe2451b403afaae5d2ecab41ae42
HHQ. Zhang <lightsing@users.noreply.github.com>
2021-08-12 15:36:14 +0800

    Fix submitter (#66)
    * do not fetch unnecessary columns from db
    
    * typo
    
    * remove reset
    
    * use commited when send task
    
    * update common-rs
    
    * require contiguous by sql
    
    * fix formatter
    
    * cargo fmt
    
    * remove committed
    
    * cargo fmt
    
    * use i64
    
    * borrow mut
    
    Co-authored-by: HAOYUatHZ <haoyu@protonmail.com>

--- src/bin/tele_out.rs
+++ src/bin/tele_out.rs
@@ -29,7 +29,7 @@ async fn main() -> anyhow::Result<()> {
     // TODO: maybe separate and have: 1. consumer 2. producer 3. sender
     let dbpool = storage::from_config(&settings).await?;
     let (tx, rx) = crossbeam_channel::unbounded();
-    let fetcher = TaskFetcher::from_config_with_pool(&settings, dbpool.clone());
+    let mut fetcher = TaskFetcher::from_config_with_pool(&settings, dbpool.clone());
     let fetcher_task_handle = tokio::spawn(async move { fetcher.run(tx).await });
     let eth_sender = EthSender::from_config_with_pool(&settings, dbpool)?;
     let eth_sender_task_handle = tokio::spawn(async move { eth_sender.run(rx).await });
