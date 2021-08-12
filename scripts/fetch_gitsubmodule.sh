ffef269319fd103d53bc52dd31665b0cb3f4c72c
HAOYUatHZ <haoyu@protonmail.com>
2021-08-12 15:40:10 +0800

    remove fetch_gitsubmodule.sh

--- scripts/fetch_gitsubmodule.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -uex
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-REPO_DIR=$DIR/".."
-cd $REPO_DIR
-
-git submodule update --init --recursive
-if [ -z ${CI+x} ]; then git pull --recurse-submodules; fi
