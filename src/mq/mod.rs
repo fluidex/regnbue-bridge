1fb9c222f7ef721223310670c784f6b2d0149348
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2021-08-12 15:53:50 +0800

    migrate to use fluidex_common for mq (#68)

--- src/mq/mod.rs
+++ src/mq/mod.rs
@@ -1,2 +1,15 @@
-pub mod messages;
-pub mod msg_consumer;
+pub mod messages {
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug)]
+    pub enum WrappedMessage {
+        User(UserMessage),
+    }
+
+    #[derive(Clone, Debug, Serialize, Deserialize)]
+    pub struct UserMessage {
+        pub user_id: i32,
+        pub l1_address: String,
+        pub l2_pubkey: String,
+    }
+}
