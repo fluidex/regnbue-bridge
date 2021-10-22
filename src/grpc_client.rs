7acc1cad4d8670b0eaa00ea3aaa9cfd1f06209d6
HAOYUatHZ <haoyu@protonmail.com>
2021-10-22 15:39:45 +0800

    bump

--- src/grpc_client.rs
+++ src/grpc_client.rs
@@ -18,6 +18,8 @@ impl GrpcClient {
             business_id: tx.id as u64,
             delta: tx.amount.to_string(),
             detail: serde_json::json!({"id": "", "faucet_tx time": tx.created_time}).to_string(),
+            log_metadata: None,
+            signature: None,
         });
         log::debug!("grpc_client sending faucet_tx (id: {:?})", tx.id);
         match client.balance_update(request).await {
@@ -26,22 +28,23 @@ impl GrpcClient {
         }
     }
 
-    pub async fn mock_transfer(&self, tx: &models::FaucetTx) -> Result<TransferResponse, anyhow::Error> {
-        assert!(tx.to_user > 1);
+    // pub async fn mock_transfer(&self, tx: &models::FaucetTx) -> Result<TransferResponse, anyhow::Error> {
+    //     assert!(tx.to_user > 1);
 
-        let mut client = matchengine_client::MatchengineClient::connect(self.upstream.clone()).await?;
+    //     let mut client = matchengine_client::MatchengineClient::connect(self.upstream.clone()).await?;
 
-        let request = tonic::Request::new(TransferRequest {
-            from: tx.to_user as u32,
-            to: (tx.to_user - 1) as u32,
-            asset: tx.asset.clone(),
-            delta: "0.1".to_string(),
-            memo: serde_json::json!({"id": "", "mock_transfer_tx time": tx.created_time}).to_string(),
-        });
-        log::debug!("grpc_client sending transfer_tx (id: {:?})", tx.id);
-        match client.transfer(request).await {
-            Ok(resp) => Ok(resp.into_inner()),
-            Err(e) => Err(anyhow!(e)),
-        }
-    }
+    //     let request = tonic::Request::new(TransferRequest {
+    //         from: tx.to_user as u32,
+    //         to: (tx.to_user - 1) as u32,
+    //         asset: tx.asset.clone(),
+    //         delta: "0.1".to_string(),
+    //         memo: serde_json::json!({"id": "", "mock_transfer_tx time": tx.created_time}).to_string(),
+    //         signature: None,
+    //     });
+    //     log::debug!("grpc_client sending transfer_tx (id: {:?})", tx.id);
+    //     match client.transfer(request).await {
+    //         Ok(resp) => Ok(resp.into_inner()),
+    //         Err(e) => Err(anyhow!(e)),
+    //     }
+    // }
 }
